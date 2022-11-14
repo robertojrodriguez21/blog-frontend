0fd575926ce4156dcfcf5b4720e41111e1ba0ccb
Edward Yuabov <yuaboved@gmail.com>
2022-11-14 11:01:43 -0500

    Added routes to login and register, added content to login and register componenets, did minor css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,15 @@
+
 const Login = () => {
+
     return(
         <div>
-
+            <form className="login-form">
+                <label>Email: </label>
+                <input/>
+                <label>Password: </label>
+                <input/>
+                <button>Log In</button>
+            </form>
         </div>
     )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,21 @@
 const Register = () => {
     return(
         <div>
-
+            <form className="form-format">
+                <label> First Name: </label>
+                <input/>
+                <label> Last Name: </label>
+                <input/>
+                <label> Enter Email Address: </label>
+                <input/>
+                <label> Enter Password: </label>
+                <input/>
+                <label> Verify Password: </label>
+                <input/>
+                <label> Location: </label>
+                <input/>
+                <button>Create Account</button>
+            </form>
         </div>
     )
 }
