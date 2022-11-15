269a03a05b6840cdca6c65126d85b018f8f6b720
Edward Yuabov <yuaboved@gmail.com>
2022-11-15 10:40:13 -0500

    Added to editPost Component

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -1,16 +1,17 @@
-const EditPost = () => {
+const EditPost = (props) => {
+    const post = props.post
     return(
         <div className="edit-post">
             <div className="edit-post-title">Edit Post</div>
-            <form className='edit-post-form'>
+            <form className='edit-post-form' onSubmit={props.handleEdit}>
                 <label>Edit Post Name: </label>
-                <input/>
+                <input required type='text' value={post.title} onChange={props.handleChange} name = {'title'}/>
                 <br/><br/>
                 <label>Edit Post: </label>
-                <input/>
+                <input required type='text' value={post.body} onChange={props.handleChange} name={'body'}/>
                 <br/><br/>
                 <label>Edit Image: </label>
-                <input/>
+                <input required type='text' value={post.body} onChange={props.handleChange} name={'body'}/>
                 <br/><br/>
                 <button className='edit-post-button'>Confirm Changes</button>
             </form>
