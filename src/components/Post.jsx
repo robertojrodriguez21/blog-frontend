24d12e2138a8087089ab627d71d7d8164744b787
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2022-11-15 09:01:33 -0600

    Added buttons to post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,13 @@
 const Post = (props) => {
     const post = props.post
+    const handleEdit = () => {
+        
+    }
+
     return(
         <div className="post">
+            <button className="delete-button" onClick={props.handleDelete()}>x</button>
+            <button className="edit-button" onClick={handleEdit()}>✎</button>
             <div className="post-image"><img src={post.image} alt={post.name} ></img></div>
             <div className="post-title">{post.title}</div>
             <div className="post-body">{post.body}</div>
