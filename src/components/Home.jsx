3aca2cab414c5b41676a6d143c7b72d6bb73cfcf
Edward Yuabov <yuaboved@gmail.com>
2022-11-15 08:26:54 -0500

    added routes to home page to make getting to the routes easier

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
-    <div>
-    
+    <div className="home">
+        <div className="home-title">ROUTES</div>    
+        <div className="home-form">  
+        <Link to = '/createPost'><button className="home-button">Create Post</button></Link>
+        <br/>
+        <br/>
+        <Link to = '/editPost'><button className="home-button">Edit Post</button></Link>
+      </div>
+
     </div>
   )
 }
