ef8743f37884e2721ddea2c8d73c4eaf7aa23dbf
Kyle Davis <kyle.davis@ucdenver.edu>
2022-11-15 11:53:21 -0700

    added security functionality to web app

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,25 @@
+import { useReducer } from 'react'
 import Post from './Post'
+import { useNavigate } from 'react-router-dom'
 
 const Feed = (props) => {
     const postsComments = props.postsComments
-    return(
+
+    let navigate = useNavigate()
+
+    return props.user && props.authenticated ? (
         <div>
             <div>FEED</div>
             {postsComments.map((postComments) => (
                 <Post key={postComments.id} postComments={postComments} />
             ))}
         </div>
-    )
+    ) : (
+        <div className="protected">
+          <h3>Oops! You must be signed in to do that!</h3>
+          <button onClick={() => navigate('/login')}>Sign In</button>
+        </div>
+      )
 }
 
 export default Feed
