cfe619eaa837832d03596ee7c66ca45ddd6a3b7d
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2022-11-16 14:13:10 -0600

    Added heading to show that there are no posts in your blog

--- src/components/MyBlog.jsx
+++ src/components/MyBlog.jsx
@@ -5,13 +5,25 @@ const MyBlog = (props) => {
 const postsComments = props.postsComments
 const user = props.user
 
+let postCounter = postsComments.length
+console.log('this is length', postCounter)
+
+const reducePostCounter = () => {
+  postCounter -= 1
+  console.log(postCounter)
+  if (postCounter === 0) {
+    console.log('hit')
+    return <h1>Add a Post to See This Page!</h1>
+  } 
+}
+
   return (
   <div>
     
     {postsComments.map((postComments) => (
-          user.id === postComments.userId ? <Post key={postComments.id} postComments={postComments} user={props.user} postUserId={postComments.userId} BASE_URL={props.BASE_URL}/>
-          : null
-          ))}
+      user.id === postComments.userId ? <Post key={postComments.id} postComments={postComments} user={props.user} postUserId={postComments.userId} BASE_URL={props.BASE_URL}/>
+      : reducePostCounter()
+    ))}
   </div>
   )
 }
