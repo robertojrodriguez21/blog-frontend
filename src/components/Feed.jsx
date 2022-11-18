e3bb3b543b7e1fb4c5e12d152389aa4bd1a322d4
Roberto J Rodriguez <robertojrodriguez21@gmail.com>
2022-11-18 11:12:01 -0600

    Repositioned footer

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,7 +6,7 @@ const Feed = (props) => {
 
     // Return
     return (
-        <div>
+        <div className='feed-container'>
             <CreatePost user={props.user} BASE_URL={props.BASE_URL} />
             {postsComments.map((postComments) => (
                 <Post key={postComments.id} postComments={postComments} user={props.user} postUserId={postComments.userId} BASE_URL={props.BASE_URL}/>
